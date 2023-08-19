51b06a772cbd071f9709037dc79521b8c1040cda
Kumari Riya <riyachouhan.ee@gmail.com>
2023-08-19 12:29:53 +0530

    ff

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ import cors from 'cors';
 import DBConnection from './database/db.js';
 
 const app = express();
-
+const PORT = process.env.PORT || 8000;  // Use the provided port or default to 8000
 
 app.use(cors());
-app.use('/', router);   
+app.use(express.json());  // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded request bodies
 
+app.use('/', router);
 
-const PORT = 8000;
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
 
-
-
-DBConnection();
-
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+// Database Connection
+DBConnection()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on PORT ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error("Database connection error:", error);
+    });
